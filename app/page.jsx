e07f713fcd2498537d90bf9b4c8913fc22682dbc
Sephiroth <Sephiroth@DESKTOP-AIJ7EIC>
2024-04-22 10:17:52 +0330

    Refact and config project

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <p>
+        loren 50 sdjhflsjdflkj
+        klsjdlkfjsldfkjlskdjflksjdlkfjslkdjflsjdflksjdlkfjslkdjf
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
